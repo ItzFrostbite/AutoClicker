d354ef24bb5a118c356aee996870506abd3f17bf
Charles <46288486+ItzFrostbiteTv@users.noreply.github.com>
2019-03-05 22:08:53 -0600

    Add files via upload

---
+++ AutoClicker.py
@@ -0,0 +1,32 @@
+from pynput.mouse import Button, Controller
+from time import sleep
+import keyboard
+import math
+
+passme = True
+pyn = Controller()
+print("What is the time gap between each click (Can be a decimal)")
+num = float(input())
+print("Press Left Control To Start/Stop")
+
+print(num)
+def check():
+    if keyboard.is_pressed('w'):
+        print("Quiting...")
+        sleep(3)
+        quit()
+
+
+while True:
+    if keyboard.is_pressed('s'):
+        print("Starting...")
+        sleep(1)
+        for i in range(1,10000):
+            check()
+            sleep(num)
+            check()
+            pyn.press(Button.left)
+            pyn.release(Button.left)
+            check()
+            print(i)
+            check()
